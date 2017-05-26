80c28b4fd71aa8c0d9f6beb901fb025e110a5296
albuemanuel <albuemanuel@live.com>
2017-05-26 19:12:07 +0300

    Calculate rate for the first month

--- BanKRate/BanKRate/BankRateTests.cs
+++ BanKRate/BanKRate/BankRateTests.cs
@@ -8,8 +8,15 @@ namespace BanKRate
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RateForFirstMonth()
         {
+            decimal rate = CalculateBankRate(200, 2, 12, 1);
+            Assert.AreEqual(102, rate);
+        }
+
+        decimal CalculateBankRate(decimal total, int periodInMonths, decimal interestPerYear, int currentMonth)
+        {
+            return total / periodInMonths + total * interestPerYear / 12 / 100;
         }
     }
 }
