12f425863cef54f02f4e5e1b334f720de5efa54e
albuemanuel <albuemanuel@live.com>
2017-05-26 19:02:08 +0300

    Add empty BankRate project

---
+++ BanKRate/BanKRate/BankRateTests.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BanKRate
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
