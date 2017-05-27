e39343ed28a350636c57c4243a4f2c5eef8ce5b4
albuemanuel <albuemanuel@live.com>
2017-05-27 14:51:10 +0300

    Extract quantity of food per goat variable

--- BanKRate/Goats/UnitTest.cs
+++ BanKRate/Goats/UnitTest.cs
@@ -10,13 +10,14 @@ namespace Goats
         [TestMethod]
         public void QuantityOfFood()
         {
-            int quantity = CalculateFoodQuantityInKg(10, 5, 10, 10, 20);
+            float quantity = CalculateFoodQuantityInKg(10, 5, 10, 10, 20);
             Assert.AreEqual(40, quantity);
         }
 
-        int CalculateFoodQuantityInKg(int days, int nrOfGoats, int quantityOfFood, int newNrOfGoats, int newNrOfDays)
+        float CalculateFoodQuantityInKg(int days, int nrOfGoats, int quantityOfFood, int newNrOfGoats, int newNrOfDays)
         {
-            return newNrOfGoats * quantityOfFood / days * newNrOfDays / nrOfGoats;
+            float quantityOfFoodPerGoat = (float)quantityOfFood / days / nrOfGoats;
+            return newNrOfGoats * quantityOfFoodPerGoat * newNrOfDays;
         }
     }
 }
