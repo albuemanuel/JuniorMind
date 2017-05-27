cd648efa2c68b8204ee3ae396662edff2034ae44
albuemanuel <albuemanuel@live.com>
2017-05-27 14:39:03 +0300

    Calculate food quantity

--- BanKRate/Goats/UnitTest.cs
+++ BanKRate/Goats/UnitTest.cs
@@ -5,11 +5,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Goats
 {
     [TestClass]
-    public class UnitTest1
+    public class GoatsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void QuantityOfFood()
         {
+            int quantity = CalculateFoodQuantityInKg(10, 5, 10, 10, 20);
+            Assert.AreEqual(40, quantity);
+        }
+
+        int CalculateFoodQuantityInKg(int days, int nrOfGoats, int quantityOfFood, int newNrOfGoats, int newNrOfDays)
+        {
+            return newNrOfGoats * quantityOfFood / days * newNrOfDays / nrOfGoats;
         }
     }
 }
