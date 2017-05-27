30b191988ccffac476ef32c10172e8b882af605a
albuemanuel <albuemanuel@live.com>
2017-05-27 19:53:45 +0300

    Calculate number of cubic stones needed for square

--- BanKRate/SquarePavement/SquarePavementTests.cs
+++ BanKRate/SquarePavement/SquarePavementTests.cs
@@ -5,11 +5,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SquarePavement
 {
     [TestClass]
-    public class UnitTest1
+    public class SquarePavementTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NoOfCubicStonesNeeded()
         {
+            int number = CalculateNoOfCubicStones(9, 6, 4);
+            Assert.AreEqual(6, number);
+        }
+
+        int CalculateNoOfCubicStones(int lengthOfSquare, int widthOfSquare, int sizeOfStone)
+        {
+            if (lengthOfSquare <= sizeOfStone && widthOfSquare <= sizeOfStone)
+                return 1;
+
+
+
+            if (lengthOfSquare <= sizeOfStone && widthOfSquare > sizeOfStone)
+                return 1 + CalculateNoOfCubicStones(lengthOfSquare, widthOfSquare - sizeOfStone, sizeOfStone);
+
+            if (lengthOfSquare > sizeOfStone && widthOfSquare <= sizeOfStone)
+                return 1 + CalculateNoOfCubicStones(lengthOfSquare - sizeOfStone, widthOfSquare, sizeOfStone);
+
+            if (lengthOfSquare > sizeOfStone && widthOfSquare > sizeOfStone)
+                return 1 + CalculateNoOfCubicStones(lengthOfSquare - sizeOfStone, widthOfSquare - sizeOfStone, sizeOfStone) + CalculateNoOfCubicStones(lengthOfSquare - sizeOfStone, sizeOfStone, sizeOfStone) + CalculateNoOfCubicStones(sizeOfStone, widthOfSquare - sizeOfStone, sizeOfStone);
+
+            return 1;
+                
         }
     }
 }
