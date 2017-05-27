7165ccc32f183571769f4b50f2582268ea4de43a
albuemanuel <albuemanuel@live.com>
2017-05-27 15:08:39 +0300

    Add empty project "Mushrooms"

---
+++ BanKRate/Mushrooms/MushroomTests.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms
+{
+    [TestClass]
+    public class MushroomTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
