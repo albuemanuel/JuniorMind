6e2c90f1d480ab751eaf0d01d9f249402eee7357
albuemanuel <albuemanuel@live.com>
2017-05-29 16:38:45 +0300

    Simplified calculations and added explaining comments

--- BanKRate/SquarePavement/SquarePavementTests.cs
+++ BanKRate/SquarePavement/SquarePavementTests.cs
@@ -10,27 +10,31 @@ namespace SquarePavement
         [TestMethod]
         public void NoOfCubicStonesNeeded()
         {
-            int number = CalculateNoOfCubicStones(9, 6, 4);
-            Assert.AreEqual(6, number);
+            int number = CalculateNoOfCubicStones(6, 6, 4);
+            Assert.AreEqual(4, number);
         }
 
-        int CalculateNoOfCubicStones(int lengthOfSquare, int widthOfSquare, int sizeOfStone)
+        int CalculateNoOfCubicStones(int lengthOfSquare, int widthOfSquare, int sizeOfCubicStone)
         {
-            if (lengthOfSquare <= sizeOfStone && widthOfSquare <= sizeOfStone)
+            if (lengthOfSquare <= sizeOfCubicStone && widthOfSquare <= sizeOfCubicStone)  //size of square smaller than one cubic stone
                 return 1;
 
+            if (lengthOfSquare <= sizeOfCubicStone && widthOfSquare > sizeOfCubicStone)   //the square is less long but is wider than a cubic stone
+                if (widthOfSquare % sizeOfCubicStone == 0)
+                    return widthOfSquare / sizeOfCubicStone;
+                else
+                    return widthOfSquare / sizeOfCubicStone + 1;
 
+            if (lengthOfSquare > sizeOfCubicStone && widthOfSquare <= sizeOfCubicStone)   //the square is longer but less wide than a cubic stone
+                if (lengthOfSquare % sizeOfCubicStone == 0)
+                    return lengthOfSquare / sizeOfCubicStone;
+                else
+                    return lengthOfSquare / sizeOfCubicStone + 1;
 
-            if (lengthOfSquare <= sizeOfStone && widthOfSquare > sizeOfStone)
-                return 1 + CalculateNoOfCubicStones(lengthOfSquare, widthOfSquare - sizeOfStone, sizeOfStone);
+            //the rest of scenarios(the square is longer and wider than a cubic stone)
 
-            if (lengthOfSquare > sizeOfStone && widthOfSquare <= sizeOfStone)
-                return 1 + CalculateNoOfCubicStones(lengthOfSquare - sizeOfStone, widthOfSquare, sizeOfStone);
+            return (lengthOfSquare / sizeOfCubicStone) * (widthOfSquare / sizeOfCubicStone) + CalculateNoOfCubicStones(lengthOfSquare - (lengthOfSquare / sizeOfCubicStone) * sizeOfCubicStone, widthOfSquare, sizeOfCubicStone) + CalculateNoOfCubicStones((lengthOfSquare / sizeOfCubicStone) * sizeOfCubicStone, widthOfSquare - (widthOfSquare / sizeOfCubicStone) * sizeOfCubicStone, sizeOfCubicStone);  
 
-            if (lengthOfSquare > sizeOfStone && widthOfSquare > sizeOfStone)
-                return 1 + CalculateNoOfCubicStones(lengthOfSquare - sizeOfStone, widthOfSquare - sizeOfStone, sizeOfStone) + CalculateNoOfCubicStones(lengthOfSquare - sizeOfStone, sizeOfStone, sizeOfStone) + CalculateNoOfCubicStones(sizeOfStone, widthOfSquare - sizeOfStone, sizeOfStone);
-
-            return 1;
                 
         }
     }
