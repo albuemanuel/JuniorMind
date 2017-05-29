06b33519c4e2c6828e828365463821f9f2199f2b
albuemanuel <albuemanuel@live.com>
2017-05-29 22:26:01 +0300

    Simplifying function for calculating nr of cubic stones needed for pavement

--- BanKRate/SquarePavement/SquarePavementTests.cs
+++ BanKRate/SquarePavement/SquarePavementTests.cs
@@ -10,30 +10,15 @@ namespace SquarePavement
         [TestMethod]
         public void NoOfCubicStonesNeeded()
         {
-            int number = CalculateNoOfCubicStones(6, 6, 4);
-            Assert.AreEqual(4, number);
+            int number = CalculateNoOfCubicStones(15, 10, 4);
+            Assert.AreEqual(12, number);
         }
+
+        
 
         int CalculateNoOfCubicStones(int lengthOfSquare, int widthOfSquare, int sizeOfCubicStone)
         {
-            if (lengthOfSquare <= sizeOfCubicStone && widthOfSquare <= sizeOfCubicStone)  //size of square smaller than one cubic stone
-                return 1;
-
-            if (lengthOfSquare <= sizeOfCubicStone && widthOfSquare > sizeOfCubicStone)   //the square is less long but is wider than a cubic stone
-                if (widthOfSquare % sizeOfCubicStone == 0)
-                    return widthOfSquare / sizeOfCubicStone;
-                else
-                    return widthOfSquare / sizeOfCubicStone + 1;
-
-            if (lengthOfSquare > sizeOfCubicStone && widthOfSquare <= sizeOfCubicStone)   //the square is longer but less wide than a cubic stone
-                if (lengthOfSquare % sizeOfCubicStone == 0)
-                    return lengthOfSquare / sizeOfCubicStone;
-                else
-                    return lengthOfSquare / sizeOfCubicStone + 1;
-
-            //the rest of scenarios(the square is longer and wider than a cubic stone)
-
-            return (lengthOfSquare / sizeOfCubicStone) * (widthOfSquare / sizeOfCubicStone) + CalculateNoOfCubicStones(lengthOfSquare - (lengthOfSquare / sizeOfCubicStone) * sizeOfCubicStone, widthOfSquare, sizeOfCubicStone) + CalculateNoOfCubicStones((lengthOfSquare / sizeOfCubicStone) * sizeOfCubicStone, widthOfSquare - (widthOfSquare / sizeOfCubicStone) * sizeOfCubicStone, sizeOfCubicStone);  
+            return (int)(Math.Ceiling((double)lengthOfSquare / sizeOfCubicStone) * Math.Ceiling((double)widthOfSquare / 4));
 
                 
         }
