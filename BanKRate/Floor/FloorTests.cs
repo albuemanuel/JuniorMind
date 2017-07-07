b706954c137486aedbb33b1d84c4fc28e1d57be4
albuemanuel <albuemanuel@live.com>
2017-07-07 23:19:58 +0300

    Calculate number of  tiles needed for room flooring

--- BanKRate/Floor/FloorTests.cs
+++ BanKRate/Floor/FloorTests.cs
@@ -5,11 +5,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Floor
 {
     [TestClass]
-    public class UnitTest1
+    public class FloorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NoOfTilesNeededForRoomFlooring()
         {
+            double qty = CalculateNoOfTilesNeeded(0.425, 2, 1, 1);
+            Assert.AreEqual(1, qty);
+        }
+
+        double CalculateNoOfTilesNeeded(double lengthOfRoom, double widthOfRoom, double lengthOfFloorTile, double widthOfFloorTile)
+        {
+            return Math.Ceiling((lengthOfRoom * widthOfRoom) / (lengthOfFloorTile * widthOfFloorTile));
         }
     }
 }
