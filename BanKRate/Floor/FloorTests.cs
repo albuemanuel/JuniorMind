98acdd642ada4bbf8a090b880e2ed54192fdd511
albuemanuel <albuemanuel@live.com>
2017-07-10 14:27:35 +0300

    Extracting loss percentage and loss fraction as parameter and variable respectively

--- BanKRate/Floor/FloorTests.cs
+++ BanKRate/Floor/FloorTests.cs
@@ -8,30 +8,31 @@ namespace Floor
     public class FloorTests
     {
         [TestMethod]
-        public void NoOfTilesNeededWithoutLosses()
+        public void NoOfTilesNeededWithoutLoss()
         {
-            int qty = CalculateNoOfTilesNeeded(7, 4, 2, 2);
+            int qty = CalculateNoOfTilesNeeded(7, 4, 2, 2, 15);
             Assert.AreEqual(7, qty);
         }
 
         [TestMethod]
-        public void NoOfTilesNeededWithLosses()
+        public void NoOfTilesNeededWithLoss()
         {
-            int qty = CalculateNoOfTilesNeeded(7, 4, 4, 2);
+            int qty = CalculateNoOfTilesNeeded(7, 4, 4, 2, 15);
             Assert.AreEqual(5, qty);
         }
 
 
 
-        int CalculateNoOfTilesNeeded(int lengthOfRoom, int widthOfRoom, int lengthOfFloorTile, int widthOfFloorTile)
+        int CalculateNoOfTilesNeeded(int lengthOfRoom, int widthOfRoom, int lengthOfFloorTile, int widthOfFloorTile, int lossPercentage)
         {
+            float lossFraction = (float)lossPercentage / 100;
             int areaOfRoom = lengthOfRoom * widthOfRoom;
             int areaOfTile = lengthOfFloorTile * widthOfFloorTile;
 
             int noOfTiles = (int)Math.Ceiling((float)areaOfRoom / areaOfTile);
-            int noOfTilesWithLosses = (int)(noOfTiles + Math.Ceiling((float)0.15 * noOfTiles));
+            int noOfTilesWithLoss = (int)(noOfTiles + Math.Ceiling(lossFraction * noOfTiles));
 
-            return noOfTilesWithLosses;
+            return noOfTilesWithLoss;
         }
     }
 }
