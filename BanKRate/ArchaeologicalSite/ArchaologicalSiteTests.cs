3836a57f0b6dbe7fb5d9976c9ec1e9ba545d5981
albuemanuel <albuemanuel@live.com>
2017-07-24 16:49:58 +0300

    Add empty ArchaeologicalSite project

---
+++ BanKRate/ArchaeologicalSite/ArchaologicalSiteTests.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArchaeologicalSite
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
