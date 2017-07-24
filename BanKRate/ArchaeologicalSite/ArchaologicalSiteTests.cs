4118c1db0266c843b97bc1eaac8a1516aa8b7b6c
albuemanuel <albuemanuel@live.com>
2017-07-24 18:22:16 +0300

    Calculate minimum area for building

--- BanKRate/ArchaeologicalSite/ArchaologicalSiteTests.cs
+++ BanKRate/ArchaeologicalSite/ArchaologicalSiteTests.cs
@@ -8,8 +8,15 @@ namespace ArchaeologicalSite
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MinArea()
         {
+            double minArea = CalculateMinArea(1, 0, 4, 0, 2.5f, 4);
+            Assert.AreEqual(6, minArea);
+        }
+
+        double CalculateMinArea(float xPoint1, float yPoint1, float xPoint2, float yPoint2, float xPoint3, float yPoint3)
+        {
+            return Math.Abs((xPoint1*(yPoint2-yPoint3) + xPoint2*(yPoint3-yPoint1) + xPoint3*(yPoint1-yPoint2))/2);
         }
     }
 }
