e983fc2c79ebfb81887dfaaf8042fba07b4178eb
albuemanuel <albuemanuel@live.com>
2017-07-25 22:08:08 +0300

    Calculate nighttime fare for medium distance

--- BanKRate/TaxiFare/TaxiFareTests.cs
+++ BanKRate/TaxiFare/TaxiFareTests.cs
@@ -31,6 +31,12 @@ namespace TaxiFare
             Assert.AreEqual(7, CalculateTaxiFare(1, 21));
         }
 
+        [TestMethod]
+        public void NighttimeFareForMediumDistances()
+        {
+            Assert.AreEqual(210, CalculateTaxiFare(21, 21));
+        }
+
 
         decimal CalculateTaxiFare(int distanceInKm, int hour)
         {
@@ -45,7 +51,7 @@ namespace TaxiFare
         private decimal[] GetPrices(int hour)
         {
             decimal[] daytimePrices = { 5, 8, 6 };
-            decimal[] nighttimePrices = { 7 };
+            decimal[] nighttimePrices = { 7, 10 };
             return IsDaytime(hour) ? daytimePrices : nighttimePrices;
         }
 
