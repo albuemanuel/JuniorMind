4d4d6971979f47ee5c7fb091206794ef461d7f6c
albuemanuel <albuemanuel@live.com>
2017-07-27 16:45:53 +0300

    Determine if melon is divisible in even number of kg

--- BanKRate/Melon/MelonTests.cs
+++ BanKRate/Melon/MelonTests.cs
@@ -5,11 +5,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Melon
 {
     [TestClass]
-    public class UnitTest1
+    public class MelonTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Divisableness()
         {
+            Assert.AreEqual(false, IsMelonDivisibleInEvenNoOfKg(11));
+        }
+
+        bool IsMelonDivisibleInEvenNoOfKg(int melonWeightInKg)
+        {
+            if(melonWeightInKg < 4)
+                return false;
+
+            if (melonWeightInKg % 2 == 0)
+                return true;
+
+            return false;
+       
         }
     }
 }
