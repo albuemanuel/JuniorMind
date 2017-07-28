bcc880565636ec25b3d42b7853e85525b4de6284
albuemanuel <albuemanuel@live.com>
2017-07-28 23:11:21 +0300

    Add empty FarmersLand project

---
+++ BanKRate/FarmersLand/FarmersLandTests.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmersLand
+{
+    [TestClass]
+    public class FarmersLandTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
