7d9ac5d4227392c2a6043d276489cf677e54da4b
albuemanuel <albuemanuel@live.com>
2017-08-02 23:44:26 +0300

    Calculate rent for long no of days past due date

--- BanKRate/RentPayments/RentPaymentsTests.cs
+++ BanKRate/RentPayments/RentPaymentsTests.cs
@@ -14,21 +14,59 @@ namespace RentPayments
         }
 
         [TestMethod]
-        public void RentForShortNrOfDaysPastDueDate()
+        public void RentForShortNoOfDaysPastDueDate()
         {
             Assert.AreEqual(208, CalculateRent(200, 2));
         }
 
         [TestMethod]
-        public void RentForMediumNrOfDaysPastDueDate()
+        public void RentForMediumNoOfDaysPastDueDate()
         {
             Assert.AreEqual(310, CalculateRent(200, 11));
         }
 
-        decimal CalculateRent(decimal rent, int nrOfDaysPastDue)
+        [TestMethod]
+        public void RentForLongNoOfDaysPastDueDate()
         {
-            decimal penalty = nrOfDaysPastDue > 10 ? 0.05m : 0.02m;
-            return rent + nrOfDaysPastDue*penalty*rent;
+            Assert.AreEqual(820, CalculateRent(200, 31));
+        }
+
+        decimal CalculateRent(decimal rent, int noOfDaysPastDue)
+        {
+            float penalty = GetPenalty(noOfDaysPastDue);
+            return rent + (noOfDaysPastDue * (decimal)penalty * rent);
+        }
+
+        private static float GetPenalty(int noOfDaysPastDue)
+        {
+            float[] penalties = { 0.02f, 0.05f, 0.1f};
+
+            if (IsShortNoOfDaysPastDueDate(noOfDaysPastDue))
+                return penalties[0];
+            if (IsMediumNoOfDaysPastDueDate(noOfDaysPastDue))
+                return penalties[1];
+            if (IsLongNoOfDaysPastDueDate(noOfDaysPastDue))
+                return penalties[2];
+            
+
+            return 0;
+
+
+        }
+
+        private static bool IsLongNoOfDaysPastDueDate(int noOfDaysPastDue)
+        {
+            return noOfDaysPastDue > 30 && noOfDaysPastDue < 40;
+        }
+
+        private static bool IsShortNoOfDaysPastDueDate(int noOfDaysPastDue)
+        {
+            return noOfDaysPastDue > 0 && noOfDaysPastDue < 11;
+        }
+
+        private static bool IsMediumNoOfDaysPastDueDate(int noOfDaysPastDue)
+        {
+            return noOfDaysPastDue > 10 && noOfDaysPastDue < 31;
         }
     }
 }
