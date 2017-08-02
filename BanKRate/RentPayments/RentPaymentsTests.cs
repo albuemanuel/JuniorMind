36d006b56981f69a9312d143b4552577db983eb2
albuemanuel <albuemanuel@live.com>
2017-08-02 22:42:17 +0300

    Add empty RentPayments project

---
+++ BanKRate/RentPayments/RentPaymentsTests.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RentPayments
+{
+    [TestClass]
+    public class RentPaymentsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
