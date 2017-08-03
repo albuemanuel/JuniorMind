d1e63049f9ba0e27d6139a91cc907e86cd8aa9d3
albuemanuel <albuemanuel@live.com>
2017-08-03 22:32:00 +0300

    Convert short numbers to roman numbers

--- BanKRate/RomanNumbers/RomanNumbersTests.cs
+++ BanKRate/RomanNumbers/RomanNumbersTests.cs
@@ -8,9 +8,46 @@ namespace RomanNumbers
     public class RomanNumbersTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ConversionForSmallNumbers()
         {
+            Assert.AreEqual("X", ConvertToRomanNumber(10));
+        }
 
+        string ConvertToRomanNumber(int no)
+        {
+            int temp = no;
+
+            if (temp == 10)
+                return "X";
+            else if (temp >= 5)
+                if (temp > 8)
+                    return "IX";
+                else
+                {
+                    temp -= 5;
+                    return "V" + ConvertToRomanNumber(temp);
+                }
+            else if (temp > 3)
+                return "IV";
+            else
+            {
+                switch(temp)
+                {
+                    case 1:
+                        return "I";
+                    case 2:
+                        return "II";
+                    case 3:
+                        return "III";
+                    default:
+                        return null;
+                        
+                }
+                    
+
+                    
+            }
+            return null;
         }
     }
 }
