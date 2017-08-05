5dfa207dfc90607448c26375690bd3581e468556
albuemanuel <albuemanuel@live.com>
2017-08-05 15:03:00 +0300

    Decompose numbers into units and tens

--- BanKRate/RomanNumbers/RomanNumbersTests.cs
+++ BanKRate/RomanNumbers/RomanNumbersTests.cs
@@ -19,89 +19,31 @@ namespace RomanNumbers
             Assert.AreEqual("XLV", ConvertToRomanNumber(45));
         }
 
-        string ConvertToRomanNumber(int no)
+        [TestMethod]
+        public void DecompositionOfNo()
         {
-            if (no == 100)
-                return "C";
-            if(no < 100)
-            {
-                if(no > 89)
-                {
-                    no -= 90;
-                    return "XC" + ConvertToRomanNumber(no);
-                }
-                else // no <= 89
-                {
-                    if(no >= 50)
-                    {
-                        no -= 50;
-                        return "L" + ConvertToRomanNumber(no);
-                    }
-                    else // no <=50
-                    {
-                        if(no > 39)
-                        {
-                            no -= 40;
-                            return "XL" + ConvertToRomanNumber(no);
-                        }
-                        else // no <= 39
-                        {
-                            if(no >= 30)
-                            {
-                                no -= 30;
-                                return "XXX" + ConvertToRomanNumber(no);
-                            }
-                            else // no < 30
-                            {
-                                if(no >= 20)
-                                {
-                                    no -= 20;
-                                    return "XX" + ConvertToRomanNumber(no);
-                                }
-                                else // no < 20
-                                {
-                                    if(no >= 10)
-                                    {
-                                        no -= 10;
-                                        return "X" + ConvertToRomanNumber(no);
-                                    }
-                                    else // no < 10
-                                    {
-                                        if(no > 8)
-                                        {
-                                            no -= 9;
-                                            return "IX";
-                                        }
-                                        else // no < 8
-                                        {
-                                            if(no >=5 )
-                                            {
-                                                no -= 5;
-                                                return "V" + ConvertToRomanNumber(no);
-                                            }
-                                            else // no < 5
-                                                switch (no)
-                                                {
-                                                    case 4:
-                                                        return "IX";
-                                                    case 3:
-                                                        return "III";
-                                                    case 2:
-                                                        return "II";
-                                                    case 1:
-                                                        return "I";
-                                                    default:
-                                                        return null;
-                                                }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return null;
+            CollectionAssert.AreEqual(new int[]{ 5, 7}, ConvertToRomanNumber(57));
+        }
+
+        int[] ConvertToRomanNumber(int no)
+        {
+            int[] noDecomposed = DecomposeNo(no);
+            string[] noRoman = { "I", "II", "III", "IV", "V", "IX", "X", "XL", "L", "XC", "X" };
+
+            
+
+            return noDecomposed;
+        }
+
+        private int[] DecomposeNo(int no)
+        {
+            int[] noDecomposed = { 0, 0 };
+
+            noDecomposed[0] = no / 10;
+            noDecomposed[1] = no % (noDecomposed[0]*10);
+
+            return noDecomposed;
+
         }
     }
 }
