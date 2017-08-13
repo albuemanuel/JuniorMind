65b627dd2039927f050a2970b660780f914710fd
albuemanuel <albuemanuel@live.com>
2017-08-13 14:35:43 +0300

    Add empty Prefix project

---
+++ BanKRate/Prefix/PrefixTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prefix
+{
+    [TestClass]
+    public class PrefixTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
