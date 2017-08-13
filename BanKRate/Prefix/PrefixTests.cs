c79b720bf79d15a9035e999f226d2cff1cfe0a5b
albuemanuel <albuemanuel@live.com>
2017-08-13 16:18:39 +0300

    Find common prefix of two strings

--- BanKRate/Prefix/PrefixTests.cs
+++ BanKRate/Prefix/PrefixTests.cs
@@ -7,8 +7,26 @@ namespace Prefix
     public class PrefixTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Prefix()
         {
+            Assert.AreEqual("abc", FindPrefix("abcdar", "abcpoate"));
+        }
+
+        string FindPrefix(string first, string second)
+        {
+            int maxLength = first.Length>second.Length ? first.Length : second.Length;
+            string prefix = "";
+
+            for(int i=0; i<maxLength; i++)
+            {
+
+                if (first[i] != second[i])
+                    break;
+                prefix += first[i];
+
+            }
+
+            return prefix;
         }
     }
 }
