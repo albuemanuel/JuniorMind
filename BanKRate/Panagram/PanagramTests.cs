2c2709a0c6fdacdea5b1b32e2061608199f3fe89
albuemanuel <albuemanuel@live.com>
2017-08-15 21:16:25 +0300

    Determine if phrase is panagram

--- BanKRate/Panagram/PanagramTests.cs
+++ BanKRate/Panagram/PanagramTests.cs
@@ -7,8 +7,27 @@ namespace Panagram
     public class PanagramTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Panagram()
         {
+            Assert.AreEqual(true, IsPanagram("The quick brown fox jumps over the lazy dog"));
+        }
+
+        bool IsPanagram(string phrase)
+        {
+            phrase = phrase.Replace(" ", "").ToLower();
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            int[] index = new int[26];
+
+            for (int i = 0; i < phrase.Length; i++)
+                for (int j = 0; j < 26; j++)
+                    if (phrase[i] == alphabet[j])
+                        index[j] = 1;
+
+            for (int i = 0; i < 26; i++)
+                if (index[i] == 0)
+                    return false;
+
+            return true;
         }
     }
 }
