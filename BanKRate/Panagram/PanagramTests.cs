c364cbfa0de416967b2832e30ea4828b20592b8e
albuemanuel <albuemanuel@live.com>
2017-08-16 09:31:32 +0300

    Revert "Optimizing algorithm"
    This reverts commit 4461743bd4e501716dfed95227f6e1110960fcdd.

--- BanKRate/Panagram/PanagramTests.cs
+++ BanKRate/Panagram/PanagramTests.cs
@@ -14,15 +14,18 @@ namespace Panagram
 
         bool IsPanagram(string phrase)
         {
-            phrase = phrase.Replace(" ", "");
-
-
+            phrase = phrase.Replace(" ", "").ToLower();
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            int[] index = new int[26];
 
             for (int i = 0; i < phrase.Length; i++)
-                if (!Char.IsLetter(phrase[i]))
-                    return false;
+                for (int j = 0; j < 26; j++)
+                    if (phrase[i] == alphabet[j])
+                        index[j] = 1;
 
-            
+            for (int i = 0; i < 26; i++)
+                if (index[i] == 0)
+                    return false;
 
             return true;
         }
