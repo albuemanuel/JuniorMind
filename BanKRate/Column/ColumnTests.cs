6d6e4a57a952b6a6f20492e6768e1af7833d18a9
albuemanuel <albuemanuel@live.com>
2017-08-17 18:50:40 +0300

    Adding empty Column project

---
+++ BanKRate/Column/ColumnTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Column
+{
+    [TestClass]
+    public class ColumnTests
+    {
+        [TestMethod]
+        public void Index()
+        {
+            Assert.AreEqual("AA", ConvertToIndex(27));
+        }
+
+        string ConvertToIndex(int no)
+        {
+            return "";
+        }
+    }
+}
---
+++ BanKRate/Column/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Column")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Column")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3d47768-36fe-46c3-879a-4951e8af4dde")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
