e17ada35da08209ad101af5ee292a2a98fa43d61
albuemanuel <albuemanuel@live.com>
2017-08-18 16:25:57 +0300

    Calculate chance to win for six out of forty-nine

--- BanKRate/Lottery/LotteryTests.cs
+++ BanKRate/Lottery/LotteryTests.cs
@@ -7,8 +7,26 @@ namespace Lottery
     public class LotteryTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ChanceToWinForSixOutOfFortynine()
         {
+
+            Assert.AreEqual("1 / 13983816", CalculateProbabilityToWin(6));
+        }
+
+        string CalculateProbabilityToWin(int category)
+        {
+            double probabilityForSequence = 1;
+            int noOfSequences = 1;
+
+            for (int i = 0; i < category; i++)
+            {
+                probabilityForSequence *= 49 - i;
+            }
+
+            for (int i = 1; i <= category; i++)
+                noOfSequences *= i;
+
+            return "1 / " + probabilityForSequence / noOfSequences;
         }
     }
 }
