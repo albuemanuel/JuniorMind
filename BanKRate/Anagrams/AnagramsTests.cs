1222dc94c7e739f616d77f8cc097e4833fd15de1
albuemanuel <albuemanuel@live.com>
2017-09-11 18:08:40 +0300

    Calculate number of anagrams for words without repeating letters

--- BanKRate/Anagrams/AnagramsTests.cs
+++ BanKRate/Anagrams/AnagramsTests.cs
@@ -7,8 +7,26 @@ namespace Anagrams
     public class AnagramsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NoOfAnagrams()
         {
+            Assert.AreEqual(6, CalculateNoOfAnagrams("arc"));
+        }
+
+        int CalculateNoOfAnagrams(string word)
+        {
+            return (int)CalculateFactorial(word.Length);
+        }
+
+        double CalculateFactorial(int no)
+        {
+            double factorial = 1;
+
+            for (int i = 2; i <= no; i++)
+                factorial *= i;
+
+            return factorial;
         }
     }
+
+    
 }
