bd22624d79737f04bb9052e68ccfd4cfb1d6869f
albuemanuel <albuemanuel@live.com>
2017-09-11 19:55:18 +0300

    Calculate number of anagrams for any word

--- BanKRate/Anagrams/AnagramsTests.cs
+++ BanKRate/Anagrams/AnagramsTests.cs
@@ -7,14 +7,44 @@ namespace Anagrams
     public class AnagramsTests
     {
         [TestMethod]
-        public void NoOfAnagrams()
+        public void NoOfAnagramsForWordsWithoutRepeatingLetters()
         {
             Assert.AreEqual(6, CalculateNoOfAnagrams("arc"));
         }
 
+        [TestMethod]
+        public void NoOfAnagramsForAnyWord()
+        {
+            Assert.AreEqual(3, CalculateNoOfAnagrams("aac"));
+        }
+
         int CalculateNoOfAnagrams(string word)
         {
-            return (int)CalculateFactorial(word.Length);
+            string lettersInWord = "";
+            int qtyOfLetters = 0;
+            int noOfAnagramsDen = 1;
+
+            for (int i=0; i<word.Length; i++)
+            {
+                if (lettersInWord.IndexOf(word[i]) >= 0)
+                    continue;
+
+                for(int j=i+1; j<word.Length; j++)
+                {
+                    if (word[i] == word[j])
+                        qtyOfLetters++;
+                }
+                
+                lettersInWord += word[i];
+                noOfAnagramsDen *= (int)CalculateFactorial(qtyOfLetters + 1);
+                qtyOfLetters = 0;
+
+            }
+
+
+
+            return (int)CalculateFactorial(word.Length) / noOfAnagramsDen;
+
         }
 
         double CalculateFactorial(int no)
