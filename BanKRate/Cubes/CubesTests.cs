5434ff0843ebc407911f246532625154563faa5f
albuemanuel <albuemanuel@live.com>
2017-09-14 17:26:03 +0300

    Calculating the kth number which has its cube ending in 888

--- BanKRate/Cubes/CubesTests.cs
+++ BanKRate/Cubes/CubesTests.cs
@@ -7,8 +7,20 @@ namespace Cubes
     public class CubesTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TheFirstCube()
         {
+            Assert.AreEqual(192, CalculateTheKthCube(1));
+        }
+
+        [TestMethod]
+        public void AnyCube()
+        {
+            Assert.AreEqual(12692, CalculateTheKthCube(51));
+        }
+
+        int CalculateTheKthCube(int index)
+        {
+            return 10 * (19 + 25 * (index-1)) + 2;
         }
     }
 }
