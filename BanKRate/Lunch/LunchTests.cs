48f236569afdf8a0e84c7c7824a6bb73ee61ce2d
albuemanuel <albuemanuel@live.com>
2017-09-15 19:39:27 +0300

    Calculate day of first synchronization occurence

--- BanKRate/Lunch/LunchTests.cs
+++ BanKRate/Lunch/LunchTests.cs
@@ -7,8 +7,30 @@ namespace Lunch
     public class LunchTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DayOfFirstSyncOccurence()
         {
+            Assert.AreEqual(20, CalculateDayWhenLunchIsSync(5, 4));
+        }
+
+        int CalculateDayWhenLunchIsSync(int first, int second)
+        {
+            int countOne = 0;
+            int countTwo = 0;
+            int biggest = first > second ? first : second;
+            int smallest = first < second ? first : second;
+
+
+
+            while (true)
+            {
+                countOne += biggest;
+                while(countTwo<countOne)
+                {
+                    countTwo += smallest;
+                }
+                if (countOne == countTwo && countOne % biggest == 0)
+                    return countOne;
+            }
         }
     }
 }
