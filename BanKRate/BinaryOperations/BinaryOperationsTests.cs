8b77dd9f3893d5beaf7dedab5a605916f38efcef
albuemanuel <albuemanuel@live.com>
2017-09-21 01:02:11 +0300

    Adding ConvertToBinary function

--- BanKRate/BinaryOperations/BinaryOperationsTests.cs
+++ BanKRate/BinaryOperations/BinaryOperationsTests.cs
@@ -15,7 +15,17 @@ namespace BinaryOperations
 
         byte[] ConvertToBinary(byte n)
         {
-            return null;
+            string binaryRepString = Convert.ToString(n, 2);
+            byte[] binaryRepByte = new byte[binaryRepString.Length];
+
+            int count = 0;
+            foreach (char bit in binaryRepString)
+            {
+                binaryRepByte[count++] = (byte)Char.GetNumericValue(bit);
+            }
+
+            return binaryRepByte;
+
         }
     }
 }
