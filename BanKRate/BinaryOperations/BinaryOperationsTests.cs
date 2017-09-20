c0b0fc363317cdb45ea99c2e201334bdeab039fb
albuemanuel <albuemanuel@live.com>
2017-09-21 01:01:21 +0300

    Adding empty BinaryOperations project

---
+++ BanKRate/BinaryOperations/BinaryOperationsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace BinaryOperations
+{
+    [TestClass]
+    public class BinaryOperationsTests
+    {
+        [TestMethod]
+        public void BinaryRepresentation()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0, 0, 1 }, ConvertToBinary(9));
+        }
+
+        byte[] ConvertToBinary(byte n)
+        {
+            return null;
+        }
+    }
+}
---
+++ BanKRate/BinaryOperations/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BinaryOperations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BinaryOperations")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("13f81c03-3f69-43f0-9a2e-0d1093d73e8b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
