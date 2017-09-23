81d9f5d1c1217b119931a7b1d933e681661fbf26
albuemanuel <albuemanuel@live.com>
2017-09-23 13:50:43 +0300

    Adding BitwiseAND function for cases in which bit number doesn't increase

--- BanKRate/BinaryOperations/BinaryOperationsTests.cs
+++ BanKRate/BinaryOperations/BinaryOperationsTests.cs
@@ -19,6 +19,12 @@ namespace BinaryOperations
             CollectionAssert.AreEqual(new byte[] { 0, 1, 0, 1, 0, 0 }, BitwiseNOT(new byte[] { 1, 0, 1, 0, 1, 1 }));
         }
 
+        [TestMethod]
+        public void BitwiseANDWhenAddNotIncreasingNoOfBits()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 1, 1, 0 }, BitwiseAND(ConvertToBinary(4), ConvertToBinary(10)));
+        }
+
         byte[] ConvertToBinary(byte n)
         {
             string binaryRepString = Convert.ToString(n, 2);
@@ -34,9 +40,11 @@ namespace BinaryOperations
 
         }
 
+
+
         byte[] BitwiseNOT(byte[] n)
         {
-            for(int i=0; i<n.Length; i++)
+            for (int i = 0; i < n.Length; i++)
             {
                 if (n[i] == 0)
                     n[i] = 1;
@@ -48,5 +56,39 @@ namespace BinaryOperations
 
         }
 
+        byte[] BitwiseAND(byte[] a, byte[] b)
+        {
+            int smallNoLen = a.Length < b.Length ? a.Length : b.Length;
+            int bigNoLen = a.Length > b.Length ? a.Length : b.Length;
+            byte[] sum = a.Length > b.Length ? (byte[])a.Clone() : (byte[])b.Clone();
+            byte[] smallNo = a.Length < b.Length ? (byte[])a.Clone() : (byte[])b.Clone();
+            byte carry = 0;
+
+            for (int i = 0; i < smallNoLen; i++)
+            {
+                sum[GetIndex(i, bigNoLen)] += (byte)(smallNo[GetIndex(i, smallNoLen)] + carry);
+                if (sum[GetIndex(i, bigNoLen)] == 2)
+                {
+                    sum[GetIndex(i, bigNoLen)] = 0;
+                    carry = 1;
+                }
+                else
+                    carry = 0;
+            }
+            for (int i = smallNoLen; i < bigNoLen; i++)
+            {
+                sum[GetIndex(i, bigNoLen)] += carry;
+                if (sum[GetIndex(i, bigNoLen)] == 2)
+                {
+                    sum[GetIndex(i, bigNoLen)] = 0;
+                    carry = 1;
+                }
+                else
+                    carry = 0;
+            }
+            return sum;
+        }
+
+        int GetIndex(int normalIndex, int length) => length - normalIndex - 1;
     }
 }
