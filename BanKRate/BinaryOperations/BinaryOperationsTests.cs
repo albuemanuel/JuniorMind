734c615fa47f9a8d20c96f74892201e399b676c6
albuemanuel <albuemanuel@live.com>
2017-09-23 18:40:15 +0300

    Adding BitwiseOR function

--- BanKRate/BinaryOperations/BinaryOperationsTests.cs
+++ BanKRate/BinaryOperations/BinaryOperationsTests.cs
@@ -37,22 +37,45 @@ namespace BinaryOperations
             CollectionAssert.AreEqual(new byte[] { 0, 1, 1, 0 }, BitwiseAND(ConvertToBinary(14), ConvertToBinary(6)));
         }
 
+        [TestMethod]
+        public void BitwiseOR()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 1, 1 }, BitwiseOR(ConvertToBinary(4), ConvertToBinary(7)));
+        }
+
+        byte[] BitwiseOR(byte[] a, byte[] b)
+        {
+            byte[] result = a.Length > b.Length ? (byte[])a.Clone() : (byte[])b.Clone();
+            int resultLen = result.Length;
+            byte[] smallNo = a.Length < b.Length ? (byte[])a.Clone() : (byte[])b.Clone();
+            int smallNoLen = smallNo.Length;
+
+            for(int i=0; i<resultLen; i++)
+            {
+                if (result[GetIndex(i, resultLen)] == 1 || smallNo[GetIndex(i, smallNoLen)] == 1)
+                    result[GetIndex(i, resultLen)] = 1;
+                if (i >= smallNoLen)
+                    result[GetIndex(i, resultLen)] = 0;
+            }
+            return result;
+        }
+
         byte[] BitwiseAND(byte[] a, byte[] b)
         {
-            byte[] sum = a.Length > b.Length ? (byte[])a.Clone() : (byte[])b.Clone();
-            int sumLen = sum.Length;
+            byte[] result = a.Length > b.Length ? (byte[])a.Clone() : (byte[])b.Clone();
+            int sumLen = result.Length;
             byte[] smallNo = a.Length < b.Length ? (byte[])a.Clone() : (byte[])b.Clone();
             int smallNoLen = smallNo.Length;
 
             for (int i = 0; i < sumLen; i++)
             {
                 if (i >= smallNoLen)
-                    sum[GetIndex(i, sumLen)] = 0;
+                    result[GetIndex(i, sumLen)] = 0;
                 else
-                    sum[GetIndex(i, sumLen)] *= smallNo[GetIndex(i, smallNoLen)];
+                    result[GetIndex(i, sumLen)] *= smallNo[GetIndex(i, smallNoLen)];
             }
 
-            return sum;
+            return result;
         }
 
         byte[] ConvertToBinary(byte n)
