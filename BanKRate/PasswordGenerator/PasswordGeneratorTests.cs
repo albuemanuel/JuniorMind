0bff7daba0271d9e878a87050b1fadf997ab7fb5
albuemanuel <albuemanuel@live.com>
2017-10-17 21:29:15 +0300

    Adding empty PasswordGenerator project

---
+++ BanKRate/PasswordGenerator/PasswordGeneratorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PasswordGenerator
+{
+    [TestClass]
+    public class PasswordGeneratorTests
+    {
+        [TestMethod]
+        public void OnlyLowercaseLettersPassword()
+        {
+            Assert.IsTrue(GeneratePassword(PasswordFormat.Lowercase).Any(char.IsLower));
+        }
+
+        [Flags]
+        enum PasswordFormat
+        {
+            Lowercase = 1,
+            Uppercase = 2
+        }
+
+        string GeneratePassword(PasswordFormat format)
+        {
+            return "";
+        }
+    }
+
+
+}
---
+++ BanKRate/PasswordGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PasswordGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PasswordGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08a078bb-7ab9-42d4-ab09-159755e275a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
