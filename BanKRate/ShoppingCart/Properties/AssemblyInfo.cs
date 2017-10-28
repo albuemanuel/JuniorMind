17de4e0e05e4e80dc7c87e0440bfed82b6e7ef6f
albuemanuel <albuemanuel@live.com>
2017-10-28 20:09:21 +0300

    Adding ShoppingCart project Adding method: AddProduct

---
+++ BanKRate/ShoppingCart/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShoppingCart")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShoppingCart")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("336afa05-498c-4949-894b-fdc6d99e5c8d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BanKRate/ShoppingCart/ShopingCartTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingCart
+{
+    [TestClass]
+    public class ShopingCartTests
+    {
+
+        [TestMethod]
+        public void AddProduct()
+        {
+            ShoppingCart cart = new ShoppingCart();
+            cart.AddProduct(new Product(30, "Cozonac"));
+            cart.AddProduct(new Product(20, "Mamaliga"));
+
+            Assert.AreEqual("cozonac, mamaliga", cart.GetProdsNames());
+        }
+
+        struct Product
+        {
+            decimal price;
+            string name;
+
+            public Product(decimal price, string name)
+            {
+                this.price = price;
+                this.name = name.ToLower();
+            }
+
+            public decimal GetPrice()
+            {
+                return price;
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+        }
+
+        struct ShoppingCart
+        {
+            Product[] prods;
+            decimal total;
+
+            public void AddProduct(Product prod)
+            {
+                if (IsEmpty())
+                    prods = new Product[] { prod };
+                else
+                {
+                    Array.Resize(ref prods, prods.Length + 1);
+                    prods[prods.Length - 1] = prod;
+                }
+
+            }
+
+            public decimal GetTotal()
+            {
+                foreach (Product p in prods)
+                    total += p.GetPrice();
+                return total;
+            }
+
+            bool IsEmpty()
+            {
+                return prods == null;
+            }
+
+            bool IsLastProduct(int index)
+            {
+                return index == prods.Length - 1;
+            }
+
+            public string GetProdsNames()
+            {
+
+                string result = "";
+
+                if (IsEmpty())
+                    return "Shopping cart empty";
+
+                for (int i = 0; i < prods.Length; i++)
+                {
+                    if (IsLastProduct(i))
+                        result += prods[i].GetName();
+                    else
+                        result += prods[i].GetName() + ',' + ' ';
+                }
+                
+                return result;
+            }
+
+        }
+        
+
+        
+        
+    }
+}
