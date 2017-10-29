a5419e625ef00ef016ac0d21ba543be6821e59d1
albuemanuel <albuemanuel@live.com>
2017-10-29 23:34:57 +0200

    Adding empty project "Alarm"

---
+++ BanKRate/Alarm/AlarmTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Alarm
+{
+    [TestClass]
+    public class AlarmTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(TriggerAlarm());
+        }
+
+        bool TriggerAlarm()
+        {
+            return false;
+        }
+    }
+
+   
+}
---
+++ BanKRate/Alarm/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Alarm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Alarm")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53177cce-26ef-4882-9071-0882292900f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
