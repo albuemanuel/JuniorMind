e6b9026f12522c504cdeb35d8ad993019205e6ba
albuemanuel <albuemanuel@live.com>
2017-10-30 00:06:10 +0200

    Adding enum Days Adding struct AlarmSettings

--- BanKRate/Alarm/AlarmTests.cs
+++ BanKRate/Alarm/AlarmTests.cs
@@ -12,6 +12,34 @@ namespace Alarm
             Assert.IsTrue(TriggerAlarm());
         }
 
+        
+
+        [Flags]
+        enum Days
+        {
+            Mon = 1,
+            Tue = 2,
+            Wed = 4,
+            Thu = 8,
+            Fri = 16,
+            Sat = 32,
+            Sun = 64
+        }
+
+        struct AlarmSettings
+        {
+            Days days;
+            byte hour;
+            byte minutes;
+
+            public AlarmSettings(Days days, byte hour, byte minutes)
+            {
+                this.days = days;
+                this.hour = hour;
+                this.minutes = minutes;
+            }
+        }
+
         bool TriggerAlarm()
         {
             return false;
