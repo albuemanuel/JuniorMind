cb722d54dca309fa90fb65bb078aeb66f5d498be
albuemanuel <albuemanuel@live.com>
2017-10-30 23:45:09 +0200

    Adding empty project "Cyclometer"

---
+++ BanKRate/Cyclometer/CyclometerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cyclometer
+{
+    [TestClass]
+    public class CyclometerTests
+    {
+        [TestMethod]
+        public void TotalDistance()
+        {
+            Assert.AreEqual(10, CalculateTotalDistance(new BicycleReadings[] { new BicycleReadings(new int[] { 10, 20, 30, 20, 10 }, 10), new BicycleReadings(new int[]{5, 10, 15, 10, 5}, 20) }));
+        }
+
+        struct BicycleReadings
+        {
+            int[] rotations;
+            byte diameter;
+
+            public BicycleReadings(int[] rotations, byte diameter)
+            {
+                this.rotations = rotations;
+                this.diameter = diameter;
+            }
+        }
+
+        int CalculateTotalDistance(BicycleReadings[] bicycles)
+        {
+            return 0;
+        }
+    }
+}
---
+++ BanKRate/Cyclometer/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cyclometer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cyclometer")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("136d9397-2da3-4a93-92a4-5d03adffbf19")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
