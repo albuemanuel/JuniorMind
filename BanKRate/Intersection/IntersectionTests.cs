63aedd6842005f92c3c07a74c74982cde508e0aa
albuemanuel <albuemanuel@live.com>
2017-11-01 15:26:51 +0200

    Adding project "Intersection"

---
+++ BanKRate/Intersection/IntersectionTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Intersection
+{
+    [TestClass]
+    public class IntersectionTests
+    {
+        [TestMethod]
+        public void FirstIntersectionPoint()
+        {
+            Section section1 = new Section(new byte[] { 0, 0, 0, 1, 1, 1 });
+            Section section2 = new Section(new byte[] { 1, 1, 1, 0, 0, 0 });
+            Section[] sections = new Section[] { section1, section2 };
+
+            Assert.AreEqual(new Point(3,3), DetermineFirstIntersectionPoint(new Map(sections)));
+        }
+
+        struct Section
+        {
+            byte[] section;
+
+
+            public Section(byte[] section)
+            {
+                this.section = section;
+
+            }
+        }
+
+        struct Map
+        {
+            Section[] sections;
+            
+            public Map(Section[] sections)
+            {
+                this.sections = sections;
+            }
+           
+        }
+
+        struct Point
+        {
+            byte x;
+            byte y;
+
+            public Point(byte x, byte y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        Point DetermineFirstIntersectionPoint(Map map)
+        {
+            return new Point(0, 0);
+        }
+
+    }
+}
---
+++ BanKRate/Intersection/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Intersection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Intersection")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e511e120-85fd-4e6b-b9e3-653f5eca1448")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
