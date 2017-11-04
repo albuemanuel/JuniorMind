698272c9b23055fe3b8745ccc06886dd51e708ad
albuemanuel <albuemanuel@live.com>
2017-11-04 14:35:16 +0200

    Adding empty project StringReversal

---
+++ BanKRate/StringReversal/StringReversalTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringReversal
+{
+    [TestClass]
+    public class StringReversalTests
+    {
+        [TestMethod]
+        public void StringReverse()
+        {
+            Assert.AreEqual("nadgob", ReverseString("bogdan"));
+        }
+
+        string ReverseString(string str)
+        {
+            return "";
+        }
+    }
+}
