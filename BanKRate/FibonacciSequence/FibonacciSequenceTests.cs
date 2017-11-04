44fb914a5d34dd28f8ef1a8adb10ad412affe7db
albuemanuel <albuemanuel@live.com>
2017-11-04 14:32:07 +0200

    Adding project Fibonacci

---
+++ BanKRate/FibonacciSequence/FibonacciSequenceTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciSequence
+{
+    [TestClass]
+    public class FibonacciSequenceTests
+    {
+        [TestMethod]
+        public void Fibonacci()
+        {
+            Assert.AreEqual(5, Fibonacci(5));
+        }
+
+        int Fibonacci(int n)
+        {
+            int prev = 0;
+            return Fibonacci(n, ref prev);
+        }
+
+        int Fibonacci(int n, ref int prev)
+        {
+            if (n < 2) return n;
+            int beforePrev = 0;
+            prev = Fibonacci(n - 1, ref beforePrev);
+            return prev + beforePrev;
+        }
+
+
+    }
+}
