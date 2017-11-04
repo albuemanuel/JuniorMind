ee66830110b0ffdd48149a4567e08e69e58c9e90
albuemanuel <albuemanuel@live.com>
2017-11-04 16:06:09 +0200

    Adding empty project "CharacterReplacement

---
+++ BanKRate/CharacterReplacement/CharacterReplacementTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CharacterReplacement
+{
+    [TestClass]
+    public class CharacterReplacementTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+    }
+}
---
+++ BanKRate/CharacterReplacement/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CharacterReplacement")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CharacterReplacement")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b86185f-5d79-4f2c-a8d4-942f66dc5c3e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
