aa6f25479dbcceecf2de9fde7e825f787d7e2cde
albuemanuel <albuemanuel@live.com>
2017-11-05 15:23:54 +0200

    Adding IsOperator f'n

---
+++ BanKRate/Calculator/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+
+    }
+}
--- BanKRate/Calculator/UnitTest1.cs
+++ BanKRate/Calculator/UnitTest1.cs
@@ -7,10 +7,27 @@ namespace Calculator
     public class CalculatorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Operator()
         {
+            Assert.IsTrue(IsOperator('+'));
+            Assert.IsFalse(IsOperator('b'));
         }
 
+        bool IsOperator(char op)
+        {
+            string operators = "+-*/";
+
+            if (operators.IndexOf(op) != -1)
+                return true;
+
+            return false;
+        }
+
+        double EvalExpr(string expr)
+        {
+            if (IsOperator(expr[0]))
+                Operation(EvalExpr(expr.Substring(1)));
+        }
 
     }
 }
