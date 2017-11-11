8ad5e5113a215080868b0b3dd02771ecc5702266
albuemanuel <albuemanuel@live.com>
2017-11-11 17:12:25 +0200

    Adding empty project "TowerOfHanoi"

---
+++ BanKRate/TowerOfHanoi/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TowerOfHanoi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TowerOfHanoi")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e98470a-2e9b-4ae4-8f6e-4250c113926b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BanKRate/TowerOfHanoi/TowerOfHanoiTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TowerOfHanoi
+{
+    [TestClass]
+    public class TowerOfHanoiTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
