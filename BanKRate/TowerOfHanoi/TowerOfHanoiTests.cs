5fe31af0a99b8d79b998f12546c89f7f48ef97be
albuemanuel <albuemanuel@live.com>
2017-11-12 20:50:37 +0200

    Add MoveDiscs f'n

--- BanKRate/TowerOfHanoi/TowerOfHanoiTests.cs
+++ BanKRate/TowerOfHanoi/TowerOfHanoiTests.cs
@@ -7,8 +7,35 @@ namespace TowerOfHanoi
     public class TowerOfHanoiTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MoveOneDisc()
         {
+            Assert.AreEqual("t1-->t3 ", MoveDiscs(1));
+        }
+
+        [TestMethod]
+        public void MoveTwoDiscs()
+        {
+            Assert.AreEqual("t1-->t2 t1-->t3 t2-->t3 ", MoveDiscs(2));
+        }
+
+        [TestMethod]
+        public void MoveThreeDiscs()
+        {
+            Assert.AreEqual("t1-->t3 t1-->t2 t3-->t2 t1-->t3 t2-->t1 t2-->t3 t1-->t3 ", MoveDiscs(3));
+        }
+
+        [TestMethod]
+        public void MoveFourDiscs()
+        {
+            Assert.AreEqual("t1-->t2 t1-->t3 t2-->t3 t1-->t2 t3-->t1 t3-->t2 t1-->t2 t1-->t3 t2-->t3 t2-->t1 t3-->t1 t2-->t3 t1-->t2 t1-->t3 t2-->t3 ", MoveDiscs(4));
+        }
+
+        string MoveDiscs(int noOfDisks, string firstTower = "t1", string secondTower = "t2", string thirdTower = "t3")
+        {
+            if (noOfDisks == 1)
+                return firstTower + "-->" + thirdTower + " ";
+
+            return MoveDiscs(noOfDisks - 1, firstTower, thirdTower, secondTower) + MoveDiscs(1, firstTower, secondTower, thirdTower) + MoveDiscs(noOfDisks - 1, secondTower, firstTower, thirdTower);
         }
     }
 }
