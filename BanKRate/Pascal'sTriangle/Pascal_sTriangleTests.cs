e08743bdcfed17d49477b918349133dc718f9830
albuemanuel <albuemanuel@live.com>
2017-11-13 21:31:30 +0200

    Work in progress...

--- BanKRate/Pascal'sTriangle/Pascal_sTriangleTests.cs
+++ BanKRate/Pascal'sTriangle/Pascal_sTriangleTests.cs
@@ -9,6 +9,34 @@ namespace Pascal_sTriangle
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(new int[2][] { new int[] { 1 }, new int[] { 1, 1 } }, GeneratePascalsTriangle(1));
+        }
+
+        struct PascalsTriangle
+        {
+            public int[][] values;
+
+            public PascalsTriangle(int level)
+            {
+                values = new int[level+1][];
+
+                for(int i=0; i<=level; i++)
+                    values[i] = new int[i + 1];
+            }
+        }
+
+        PascalsTriangle GeneratePascalsTriangle(int level)
+        {
+            PascalsTriangle paTri = new PascalsTriangle(level);
+
+            GeneratePascalsTriangle(ref paTri);
+
+            return paTri;
+        }
+
+        void GeneratePascalsTriangle(ref PascalsTriangle paTri)
+        {
+
         }
     }
 }
