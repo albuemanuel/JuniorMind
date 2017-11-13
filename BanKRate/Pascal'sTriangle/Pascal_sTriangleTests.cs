4e9f0c2a0624d78beaaa5d1ed0a46b0fd950c310
albuemanuel <albuemanuel@live.com>
2017-11-13 21:02:14 +0200

    Adding empty project "Pascal'sTriangle"

---
+++ BanKRate/Pascal'sTriangle/Pascal_sTriangleTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pascal_sTriangle
+{
+    [TestClass]
+    public class Pascal_sTriangleTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BanKRate/Pascal'sTriangle/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pascal'sTriangle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pascal'sTriangle")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1368f107-2c1d-4338-94d7-7204381b0a1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
