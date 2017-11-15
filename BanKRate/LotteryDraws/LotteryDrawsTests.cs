19e3e59a7f75a812d5cbb8835725bc35b93441fe
albuemanuel <albuemanuel@live.com>
2017-11-16 01:38:43 +0200

    Adding project "LotteryDraws" Adding SortNumbers f'n

---
+++ BanKRate/LotteryDraws/LotteryDrawsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LotteryDraws
+{
+    [TestClass]
+    public class LotteryDrawsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        int[] SortNumbers(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                        Swap(ref numbers[i], ref numbers[j]);
+                }
+
+            return numbers;
+        }
+    }
+}
---
+++ BanKRate/LotteryDraws/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LotteryDraws")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LotteryDraws")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3d68095-9b6d-44f6-bd43-3582c2ae0d5e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
