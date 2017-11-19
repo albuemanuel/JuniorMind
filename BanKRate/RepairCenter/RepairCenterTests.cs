123e51a91f20850dbe58add857ede3a2681b85b6
albuemanuel <albuemanuel@live.com>
2017-11-19 21:02:29 +0200

    Adding SortCases f'n Adding PlaceIntAtIndex f'n

--- BanKRate/RepairCenter/RepairCenterTests.cs
+++ BanKRate/RepairCenter/RepairCenterTests.cs
@@ -7,14 +7,83 @@ namespace RepairCenter
     public class RepairCenterTests
     {
         [TestMethod]
-        public void Priority()
+        public void CaseOrderByPriority()
         {
-            Assert.AreEqual(0, 0);
+            Case[] cases = new Case[] { new Case("frigider", Priority.Low), new Case("masina", Priority.High), new Case("masina de spalat", Priority.Medium), new Case("tractor", Priority.High), new Case("buldozer", Priority.Low) };
+            CollectionAssert.AreEqual(new int[] { 3, 1, 2, 0, 4 }, SortCases(cases));
         }
 
-        int[] Sort()
+        [TestMethod]
+        public void PlacementOfIntAtIndex()
         {
-            return null;
+            CollectionAssert.AreEqual(new int[] { 2, 3, 10, 5, 4 }, PlaceIntAtIndex(new int[] { 2, 3, 5, 4 }, 10, 2));
+            CollectionAssert.AreEqual(new int[] { 2, 3, 5, 10, 4 }, PlaceIntAtIndex(new int[] { 2, 3, 5, 4 }, 10, 3));
+            CollectionAssert.AreEqual(new int[] { 2, 3, 5, 4, 10 }, PlaceIntAtIndex(new int[] { 2, 3, 5, 4 }, 10, 4));
+        }
+
+        struct Case
+        {
+            public Priority priority;
+            public string description;
+
+            public Case(string description, Priority priority)
+            {
+                this.priority = priority;
+                this.description = description;
+            }
+        }
+
+        enum Priority
+        {
+            Low,
+            Medium,
+            High
+        }
+
+        int[] PlaceIntAtIndex(int[] arr, int value, int index)
+        {
+            if (arr == null)
+                return new int[1] { value };
+
+            int[] newArr = new int[arr.Length + 1];
+
+            if (index == arr.Length)
+            {
+                Array.Copy(arr, newArr, arr.Length);
+                newArr[index] = value;
+                return newArr;
+            }
+
+            Array.Copy(arr, newArr, index);
+            newArr[index] = value;
+            Array.Copy(arr, index, newArr, index + 1, arr.Length - index);
+
+            return newArr;
+        }
+
+        int[] SortCases(Case[] cases)
+        {
+            int[] indexes = new int[0];
+            int p = 0;
+
+            for(int i = 0; i<cases.Length; i++)
+            {
+                if (cases[i].priority == Priority.Low)
+                {
+                    indexes = PlaceIntAtIndex(indexes, i, indexes.Length);
+                }
+                if (cases[i].priority == Priority.Medium)
+                {
+                    indexes = PlaceIntAtIndex(indexes, i, p);
+                }
+                if(cases[i].priority == Priority.High)
+                {
+                    indexes = PlaceIntAtIndex(indexes, i, 0);
+                    p++;
+                }
+            }
+
+            return indexes;
         }
     }
 }
