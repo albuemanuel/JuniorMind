2e7cf610eea32f3401529eac5c7d34bf315cd4bd
albuemanuel <albuemanuel@live.com>
2017-11-19 14:11:22 +0200

    working...

---
+++ BanKRate/RepairCenter/RepairCenterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepairCenter
+{
+    [TestClass]
+    public class RepairCenterTests
+    {
+        [TestMethod]
+        public void Priority()
+        {
+            Assert.AreEqual(0, 0);
+        }
+
+        int[] Sort()
+        {
+            return null;
+        }
+    }
+}
