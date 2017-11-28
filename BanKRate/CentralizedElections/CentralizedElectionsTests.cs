8da438c6b1141bf550e006c5409a97160e4bce3f
albuemanuel <albuemanuel@live.com>
2017-11-28 17:10:21 +0200

    Adding PollingStation struct

--- BanKRate/CentralizedElections/CentralizedElectionsTests.cs
+++ BanKRate/CentralizedElections/CentralizedElectionsTests.cs
@@ -18,6 +18,14 @@ namespace CentralizedElections
             Assert.AreEqual("Boc: 750", new Candidate("Boc", 750).ToString());
         }
 
+        [TestMethod]
+        public void RegisteredPollingStation()
+        {
+            PollingStation pollingStation = new PollingStation(new string[] { "Boc", "Johannis", "Dragnea", "Basescu", "Becali" }, new int[] { 450, 200, 199, 399, 500 });
+
+            Assert.AreEqual("Boc: 450\nJohannis: 200\nDragnea: 199\nBasescu: 399\nBecali: 500\n", pollingStation.ToString());
+        }
+
         struct Candidate
         {
             string name;
@@ -46,10 +54,20 @@ namespace CentralizedElections
                 for (int i = 0; i < names.Length; i++)
                     candidates[i] = new Candidate(names[i], votes[i]);
             }
+
+            public override string ToString()
+            {
+                string candidates = "";
+
+                foreach (Candidate candidate in this.candidates)
+                    candidates += candidate.ToString() + '\n';
+
+                return candidates;
+            }
         }
 
 
-        Candidate[] CentralizeElections(Candidate[][] list)
+        Candidate[] CentralizeElections(PollingStation[] pollingStations)
         {
             return null;
         }
