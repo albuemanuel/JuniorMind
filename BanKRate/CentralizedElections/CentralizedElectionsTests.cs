91dab0af04d40b4a8734cf875e8a3e6efc176f06
albuemanuel <albuemanuel@live.com>
2017-11-28 15:33:15 +0200

    Adding empty project "CentralizedElections"

---
+++ BanKRate/CentralizedElections/CentralizedElectionsTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CentralizedElections
+{
+    [TestClass]
+    public class CentralizedElectionsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+    }
+}
---
+++ BanKRate/CentralizedElections/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CentralizedElections")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CentralizedElections")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7306a891-6afd-4d50-8ee5-b2c506482b27")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
