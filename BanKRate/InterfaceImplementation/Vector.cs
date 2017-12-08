1baff55dd21fd5fb4d4bb71ceee91117a975f661
albuemanuel <albuemanuel@live.com>
2017-12-08 18:38:09 +0200

    Correcting...

--- BanKRate/InterfaceImplementation/Vector.cs
+++ BanKRate/InterfaceImplementation/Vector.cs
@@ -7,36 +7,40 @@ namespace InterfaceImplementation
 {
     class Vector<T> : IList<T>
     {
-        T[] _vector;
-        int _count;
+        T[] vector;
+        int count;
         
         public Vector(int size)
         {
-            _vector = new T[size];
+            vector = new T[size];
         }
 
-        public T this[int index] { get => _vector[index]; set => _vector[index] = value; }
+        public T this[int index] { get => vector[index]; set => vector[index] = value; }
 
-        public int Count => _count;
+        public int Count => count;
 
         public bool IsReadOnly => throw new NotImplementedException();
 
+        void EnsureCapacity()
+        {
+            if (vector.Length == count)
+                Array.Resize(ref vector, vector.Length * 2);
+        }
+
         public void Add(T item)
         {
-            if (_vector.Length == _count)
-                Array.Resize(ref _vector, _vector.Length * 2);
-
-            _vector[_count++] = item;
+            EnsureCapacity();
+            vector[count++] = item;
         }
 
         public void Clear()
         {
-            _count = 0;
+            count = 0;
         }
 
         public bool Contains(T item)
         {
-            foreach (T el in _vector)
+            foreach (T el in vector)
             {
                 if (el.Equals(item))
                     return true;
@@ -46,19 +50,19 @@ namespace InterfaceImplementation
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            _vector.CopyTo(array, arrayIndex);
+            vector.CopyTo(array, arrayIndex);
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            return new VectorEnumerator<T>(_vector);
+            return new VectorEnumerator<T>(vector);
         }
 
         public int IndexOf(T item)
         {
-            for(int i=0; i<_vector.Length;i++)
+            for(int i=0; i<vector.Length;i++)
             {
-                if (_vector[i].Equals(item))
+                if (vector[i].Equals(item))
                     return i;
             }
             return -1;
@@ -66,15 +70,14 @@ namespace InterfaceImplementation
 
         public void Insert(int index, T item)
         {
-            if (index < _count && index >= 0)
+            if (index < count && index >= 0)
             {
-                if (_count == _vector.Length)
-                    Array.Resize(ref _vector, _vector.Length + 1);
+                EnsureCapacity();
 
-                for (int i = _count - 1; i > index; i--)
-                    _vector[i] = _vector[i - 1];
+                for (int i = count - 1; i > index; i--)
+                    vector[i] = vector[i - 1];
 
-                _vector[index] = item;
+                vector[index] = item;
             }
         }
 
@@ -93,28 +96,27 @@ namespace InterfaceImplementation
 
         public void RemoveAt(int index)
         {
-            if (index >= 0 && index < _count)
-            {
-                for (int i = index; i < _vector.Length - 1; i++)
-                    _vector[i] = _vector[i + 1];
+            
+            for (int i = index; i < vector.Length - 1; i++)
+                vector[i] = vector[i + 1];
 
-                _count--;
-            }
+            count--;
+            
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return _vector.GetEnumerator();
+            return vector.GetEnumerator();
         }
 
         public override string ToString()
         {
             string vectorAsString = "";
-            for(int i=0; i<_vector.Length-1; i++)
+            for(int i=0; i<vector.Length-1; i++)
             {
-                vectorAsString += _vector[i] + ", ";
+                vectorAsString += vector[i] + ", ";
             }
-            vectorAsString += _vector[_vector.Length - 1];
+            vectorAsString += vector[vector.Length - 1];
 
             return vectorAsString;
         }
