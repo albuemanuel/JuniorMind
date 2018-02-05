dc899a78da24a04b48c38148b8d10bc34858f44a
Emanuel Albu <albue@DESKTOP-7NLSNIJ>
2018-02-05 16:17:52 +0200

    Adding almost empty project "JSONParsing"...

---
+++ BanKRate/JSONParser/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONParser
+{
+    class Character : IPattern
+    {
+        char pattern;
+
+        public Character(char c)
+        {
+            pattern = c;
+        }
+
+        public(IMatch, string) Match(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+                return (new NoMoreText, text);
+            if (text[0] != pattern)
+                return (new NoMatch(text[0]), text);
+
+            return (new Success(text[0]), text.Substring(1));
+        }
+
+
+    }
+}
---
+++ BanKRate/JSONParser/JSONParserTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace JSONParser
+{
+    public class JSONParserTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
