abb9667440fbed8403f3cf12b54d70b1f013e7e5
Emanuel Albu <albue@DESKTOP-7NLSNIJ>
2018-02-13 17:06:21 +0200

    Correcting Text class

--- BanKRate/JSONParser/Text.cs
+++ BanKRate/JSONParser/Text.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace JSONParser
 {
@@ -10,13 +10,7 @@ namespace JSONParser
 
         public Text(string pattern)
         {
-            Character[] characters = new Character[pattern.Length];
-            int i = 0;
-
-            foreach(char character in pattern.ToCharArray())
-                characters[i++] = new Character(character);
-
-            this.pattern = new Sequence(characters);
+            this.pattern = new Sequence(pattern.Select(character => new Character(character)).ToArray());
         }
 
         public (IMatch, string) Match(string text)
