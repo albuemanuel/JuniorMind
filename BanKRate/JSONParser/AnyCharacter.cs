7d84f715057bd5ceb2dbeb3ba99e120ee678b4d7
Emanuel Albu <albue@DESKTOP-7NLSNIJ>
2018-03-03 18:55:45 +0200

    Using HashSet in AnyCharacter

--- BanKRate/JSONParser/AnyCharacter.cs
+++ BanKRate/JSONParser/AnyCharacter.cs
@@ -6,7 +6,7 @@ namespace JSONParser
 {
     class AnyCharacter : IPattern
     {
-        Choice pattern;
+        HashSet<Character> pattern;
 
         public AnyCharacter(string text)
         {
@@ -16,9 +16,22 @@ namespace JSONParser
             foreach (char el in text)
                 textArray[i++] = new Character(el);
 
-            pattern = new Choice(textArray);
+            pattern = new HashSet<Character>(textArray);
         }
 
-        public (IMatch, string) Match(string text) => pattern.Match(text);
+        public (IMatch, string) Match(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+                return (new NoMoreText(), text);
+
+            foreach (Character choice in pattern)
+            {
+                var (match, remainingText) = choice.Match(text);
+
+                if (match.Success)
+                    return (match, remainingText);
+            }
+            return (new NoMatch(text[0].ToString()), text);
+        }
     }
 }
