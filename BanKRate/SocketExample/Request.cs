66356d8ebb664cfcc8018a40b683f8321c7b9efb
albuemanuel <albuemanuel@live.com>
2018-03-28 21:45:03 +0300

    ...

--- BanKRate/SocketExample/Request.cs
+++ BanKRate/SocketExample/Request.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JSONParser;
 
 namespace SocketExample
 {
@@ -10,7 +11,7 @@ namespace SocketExample
     public class Request
     {
         Uri uri;
-        
+        Method method;
         float httpVersion;
         Dictionary<string, string> httpHeaderFields;
 
