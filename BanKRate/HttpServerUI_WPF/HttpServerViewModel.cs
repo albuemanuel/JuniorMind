351bff492343cc3fca63209a80931cbdd57a5887
albuemanuel <albuemanuel@live.com>
2018-04-24 20:03:31 +0300

    ...

---
+++ BanKRate/HttpServerUI_WPF/HttpServerViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpServerUI_WPF
+{
+    public class HttpServerViewModel
+    {
+        HttpServer httpServer;
+        Thread thread;
+
+        delegate void ChangeStatusBoxText();
+
+        private void StartServer(object sender, RoutedEventArgs e)
+        {
+            Int32.TryParse(PortBox.Text, out int port);
+
+            if (httpServer == null || httpServer.ShouldStop)
+            {
+                ChangeButtonState(sender as Button);
+                httpServer = new HttpServer(port, IPBox.Text, BaseURIComboBox.Text);
+                httpServer.ConsoleTextChanged += HttpServer_ConsoleTextChanged;
+                thread = new Thread(new ThreadStart(httpServer.StartHttpServer));
+                thread.Start();
+            }
+            else
+            {
+                ChangeButtonState(sender as Button);
+                httpServer.RequestStop();
+                //thread.Join(5000);
+            }
+        }
+
+        private void ChangeButtonState(Button button)
+        {
+            switch (button.Content)
+            {
+                case "Start":
+                    button.Content = "Stop";
+                    break;
+                case "Stop":
+                    button.Content = "Start";
+                    break;
+            }
+        }
+
+        private void HttpServer_ConsoleTextChanged(string text)
+        {
+            ChangeStatusBoxText del = delegate ()
+            {
+                if (!string.IsNullOrWhiteSpace(StatusBox.Text))
+                    StatusBox.AppendText("\r\n" + text);
+                else
+                    StatusBox.AppendText(text);
+            };
+            Dispatcher.Invoke(del);
+        }
+
+        private void ServerWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (httpServer != null)
+                httpServer.RequestStop();
+        }
+    }
+}
--- BanKRate/HttpServerUI_WPF/MainWindow.xaml.cs
+++ BanKRate/HttpServerUI_WPF/MainWindow.xaml.cs
@@ -21,65 +21,13 @@ namespace HttpServerUI_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        HttpServer httpServer;
-        Thread thread;
-
-        delegate void ChangeStatusBoxText();
+        
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Int32.TryParse(PortBox.Text, out int port);
-
-            if (httpServer == null || httpServer.ShouldStop)
-            {
-                ChangeButtonState(sender as Button);
-                httpServer = new HttpServer(port, IPBox.Text, BaseURIComboBox.Text);
-                httpServer.ConsoleTextChanged += HttpServer_ConsoleTextChanged;
-                thread = new Thread(new ThreadStart(httpServer.StartHttpServer));
-                thread.Start();
-            }
-            else
-            {
-                ChangeButtonState(sender as Button);
-                httpServer.RequestStop();
-                //thread.Join(5000);
-            }
-        }
-
-        private void ChangeButtonState(Button button)
-        {
-            switch (button.Content)
-            {
-                case "Start":
-                    button.Content = "Stop";
-                    break;
-                case "Stop":
-                    button.Content = "Start";
-                    break;
-            }
-        }
-
-        private void HttpServer_ConsoleTextChanged(string text)
-        {
-            ChangeStatusBoxText del = delegate ()
-            {
-                if (!string.IsNullOrWhiteSpace( StatusBox.Text))
-                    StatusBox.AppendText("\r\n" + text);
-                else
-                    StatusBox.AppendText(text);
-            };
-            Dispatcher.Invoke(del);
-        }
-
-        private void ServerWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            if(httpServer != null)
-                httpServer.RequestStop();
-        }
+        
     }
 }
